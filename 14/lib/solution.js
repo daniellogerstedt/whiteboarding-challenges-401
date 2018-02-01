00d4846a3551467c1f17a91e1eef4885bce4f62f
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-02-01 14:29:52 -0800

    finished whiteboard challenge 14

---
+++ 14/lib/solution.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let SLL = require('../assets/sll'), Stack = require('../assets/stack');
+
+
+
+module.exports = (sll) => {
+  if (!sll) return new Error('Validation Error: Argument Required');
+  if (!(sll instanceof SLL)) return new Error('Validation Error: Argument Passed Is Not A Linked-List');
+  if (!sll.head) return new Error('Validation Error: List Passed As Argument Is Empty');
+  let copy = Object.assign(sll), stack = new Stack();
+  stack.push(copy.findNthNode(1).val);
+  copy.remove(1);
+  for (let lts = copy.head; lts; lts = lts.next) {
+    if (lts.val === stack.top.val) stack.push(copy.findNthNode(1).val);
+    copy.remove(1);
+  }
+  for (let stl = stack.top; stl; stl = stack.top) {
+    copy.insertHead(stack.pop().val);
+  }
+  return copy;
+};
