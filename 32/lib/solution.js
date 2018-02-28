4023681697a9253f9e6cdddc267e900008a81a14
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-02-27 16:12:00 -0800

    finished solution and recursive solution

---
+++ 32/lib/solution.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = n => {
+  if (typeof n !== 'number') return null;
+  let cache = [0, 1];
+  switch(n) {
+  case(0): return 0;
+  case(1): return 1;
+  default: {
+    for (let i = 2;i <= n; i++) {
+      cache.push(cache[i-1]+cache[i-2]);
+    }
+    return cache[n];
+  }
+  }
+};
